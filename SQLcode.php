014db8ff9fb65438ef839a7980225616581dbf7f
Ezno <vutuandattb2002@gmail.com>
2024-04-18 19:59:42 +0700

    change

--- SQLcode.php
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE note_management;
-USE note_management;
-
-CREATE TABLE notes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
